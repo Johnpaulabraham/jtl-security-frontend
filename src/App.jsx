e3475d09ec73ebbdc2cf0de3ba5684f4b0dc123c
johnpaul abraham <johnpaulabraham0@gmail.com>
2025-07-31 17:20:24 +0100

    page setup

--- src/App.jsx
+++ src/App.jsx
@@ -4,10 +4,10 @@ function App() {
 	return (
 		<>
 			<div className='main flex flex-col items-center justify-center h-screen bg-gray-100'>
-				<h1 className='font-extrabold text-blue-900 text-5xl font-serif'>
+				<h1 className='font-extrabold text-blue-900 text-5xl font-serif text-center m-4'>
 					JTL Security & Safety Limited.
 				</h1>
-				<p className='text-2xl font-bold font-sans '>
+				<p className='text-2xl font-bold font-sans text-center m-5'>
 					This Website is under development, Thank you
 				</p>
 			</div>
